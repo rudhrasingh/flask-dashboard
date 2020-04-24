6ab6e6f632bc5f34dc4b5ecb5f3bf64828ce4b16
Surya Singh <suryasingh@Suryas-MacBook-Pro.local>
2020-04-24 18:01:23 +0530

    initial app

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,17 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
+    return 'index page'
+@app.route('/home')
+def home():
     return 'Home page'
+@app.route('/login')
+def login():
+    return 'login page'
+@app.route('/dashboard')
+def dashboard():
+    return 'dashboard page'
+
 
 if __name__=='__main__':
     app.run(host='localhost',debug=True)
