0d1d5038d411e7bf34622b42018042396a933fbb
Surya Singh <suryasingh@Suryas-MacBook-Pro.local>
2020-04-25 18:30:04 +0530

    test dashboard data

--- backend/app.py
+++ backend/app.py
@@ -14,12 +14,13 @@ def login():
     return render_template('login.html')
 @app.route('/dashboard',methods=['GET', 'POST'])
 def dashboard():
+
     csvfile = open('data.csv', 'r')
-    fieldnames = ('data','datasetname','source_filename','correlation_id',
+    fieldnames = ('date','datasetname','source_filename','correlation_id',
                   'application','local_execution_id','tracking_step','record_count','status,message')
     reader = csv.DictReader( csvfile, fieldnames)
     dashboard_data = json.dumps([row for row in reader])
-    return (dashboard_data)
+    return dashboard_data
 
 
 if __name__=='__main__':
