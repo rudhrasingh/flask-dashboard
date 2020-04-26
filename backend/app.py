499cb9516d5097e7f9714f0c5bf8b2721ce47374
Surya Singh <suryasingh@Suryas-MacBook-Pro.local>
2020-04-26 17:28:56 +0530

    reading the response data sent back from react form to be used in query filters

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,request
 import json,csv
 from flask import jsonify
 from flask_cors import CORS
@@ -11,26 +11,45 @@ CORS(app)
 @app.route('/')
 def index():
     return redirect(url_for('home'))
+
+
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
 @app.route('/login',methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
+
 @app.route('/dashboard',methods=['GET','POST'])
 def dashboard():
+    """ this is the main api getting data from the react form and sending data back to react in json format """
+
+    ''' below parameters will be used in sql query to filter data coming from the process_table in db'''
+    respone = request.json                   # getting the form data sent by form on the react front end
+    application = respone.get('application')
+    dataset = respone.get('dataset')
+    fromdate = respone.get('fromDate')
+    todate = respone.get('toDate')
+    print(application)
+    print(dataset)
 
     csvfile = open('data.csv', 'r')
-    fieldnames = ('date','datasetname','source_filename','correlation_id',
-                  'application','local_execution_id','tracking_step','record_count','status','message')
+    fieldnames = ('date','datasetname','source_filename',
+                  'correlation_id','application','local_execution_id',
+                  'tracking_step','record_count','status','message')
+
     reader = csv.DictReader( csvfile, fieldnames)
-    dashboard_data=[]
-    for  row in reader:
+    dashboard_data = []
+    for row in reader:
         dashboard_data.append(row)
+    application_list = ['Transformation','Stage load','OPAL'] # this list will be created from a table column in db
+    dataset_list = ['INT0030','INT0050','INT0060','INT0070','INT0080']  # this list will be created from a table column in db
+    drop_down_data = {'application_list': application_list,'dataset_list': dataset_list}
 
-    drop_down_data={'application_list':['Transformation','Stage load','OPAL'],'dataset_list':['INT0030','INT0050','INT0060','INT0070','INT0080']}
-
-    full_data={'dropdown_data':drop_down_data,'dashboard_data':dashboard_data} #this has to be changed later
+    full_data={'dropdown_data':drop_down_data,'dashboard_data':dashboard_data} # this has to be changed later
 
     return (full_data)
 
